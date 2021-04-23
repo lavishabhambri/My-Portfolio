a645f5e96710cff2504b719d3b0406d55b27a2ee
Lavisha Bhambri <bhambrilavisha@gmail.com>
2021-04-23 15:46:40 +0530

    Updated colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,9 +38,10 @@ sr.reveal('.home__title',{});
 sr.reveal('.button',{delay: 200});
 sr.reveal('.home__img',{delay: 400});
 sr.reveal('.home__social-icon',{ interval: 200});
+// sr.reveal('.home__img',{delay: 400});
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img',{});
+sr.reveal('.about__img',{delay: 200});
 sr.reveal('.about__subtitle',{delay: 400});
 sr.reveal('.about__text',{delay: 400});
 
@@ -57,3 +58,8 @@ sr.reveal('.blog-post',{interval: 200});
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200});
+
+/*SCROLL FOOTER*/
+sr.reveal('.footer__title',{ delay: 200});
+sr.reveal('.footer__phone',{ delay: 200});
+sr.reveal('.footer__social',{ interval: 200});
